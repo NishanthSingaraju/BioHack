08772e88683b3a10d4af859af7e8e718f9f51ec0
Nishanth Singaraju <nsingaraju@google.com>
2022-02-09 20:22:00 -0500

    added frontEnd

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ButtonAppBar from "./NavBar.js"
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonAppBar></ButtonAppBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/DailyInsight.js
@@ -0,0 +1,5 @@
+export default function DailyInsights(){
+    return (
+        <div></div>
+    );
+}
